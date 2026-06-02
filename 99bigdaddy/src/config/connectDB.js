524d678b6d1bbb2b1aa8f0a960d71e534360194d
Tanuj120 <tulsyantanuj@gmail.com>
2026-06-02 15:32:24 +0530

    Prepare deployment

--- 99bigdaddy/src/config/connectDB.js
+++ 99bigdaddy/src/config/connectDB.js
@@ -8,10 +8,10 @@ import mysql from 'mysql2/promise';
 //     database: 'lawra',
 // });
 const dbConfig = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || '',
-    password: process.env.DB_PASSWORD || '',
-    database: process.env.DB_NAME || '',
+    host: process.env.DB_HOST || process.env.DATABASE_HOST || 'localhost',
+    user: process.env.DB_USER || process.env.DATABASE_USER || '',
+    password: process.env.DB_PASSWORD || process.env.DATABASE_PASSWORD || '',
+    database: process.env.DB_NAME || process.env.DATABASE_NAME || '',
     port: Number(process.env.DB_PORT || 3306),
     waitForConnections: true,
     connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
