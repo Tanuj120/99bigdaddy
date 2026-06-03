73e596f4eb394faf887d2615668e4e05c3f94015
Tanuj120 <tulsyantanuj@gmail.com>
2026-06-04 00:55:28 +0530

    Update project

--- 99bigdaddy/src/config/connectDB.js
+++ 99bigdaddy/src/config/connectDB.js
@@ -9,11 +9,27 @@ import md5 from "md5";
 //     database: 'lawra',
 // });
 const dbConfig = {
-  host: process.env.DB_HOST || process.env.DATABASE_HOST || "localhost",
-  user: process.env.DB_USER || process.env.DATABASE_USER || "",
-  password: process.env.DB_PASSWORD || process.env.DATABASE_PASSWORD || "",
-  database: process.env.DB_NAME || process.env.DATABASE_NAME || "",
-  port: Number(process.env.DB_PORT || 3306),
+  host:
+    process.env.MYSQLHOST ||
+    process.env.DB_HOST ||
+    process.env.DATABASE_HOST ||
+    "localhost",
+  user:
+    process.env.MYSQLUSER ||
+    process.env.DB_USER ||
+    process.env.DATABASE_USER ||
+    "",
+  password:
+    process.env.MYSQLPASSWORD ||
+    process.env.DB_PASSWORD ||
+    process.env.DATABASE_PASSWORD ||
+    "",
+  database:
+    process.env.MYSQLDATABASE ||
+    process.env.DB_NAME ||
+    process.env.DATABASE_NAME ||
+    "",
+  port: Number(process.env.MYSQLPORT || process.env.DB_PORT || 3306),
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
   queueLimit: 0,
